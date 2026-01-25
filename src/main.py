a3bd8a1049b139e484b63768fc97c23e832ea604
SangJin983 <lazistar983@gmail.com>
2026-01-25 22:23:17 +0900

    feat: 업로드 API 구현
    POST /upload
    - 이미지 업로드 및 메타데이터 Redis 저장
    - uploadId 생성 (upload_ + UUID 8자리)
    - TTL 24시간
    
    GET /upload/{uploadId}
    - 메타데이터 조회
    - 404 처리
    
    - UploadService로 비즈니스 로직 분리
    - lifespan으로 Redis 연결 관리

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from src.routes.upload import router as upload_router
+from src.utils.redis import close_redis
 from src.utils.storage import get_storage
 from src.utils.storage.local import LocalStorage
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+    yield
+    await close_redis()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(upload_router)
 
 # LocalStorage인 경우에만 StaticFiles 마운트 (S3 전환 시 제거)
 storage = get_storage()
